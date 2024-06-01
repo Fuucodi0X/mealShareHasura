3e1fd8ba20589000d64f452e83c32f4fe11a401d
Fuucodi0X <akmelf20@gmail.com>
2024-06-01 21:42:58 +0300

    updated db(version 2)

--- db_shema/Recipes.sql
+++ db_shema/Recipes.sql
@@ -1,71 +1,92 @@
--- Creating tables and relaiton for mealshare (init project)
+CREATE TABLE "users" (
+  "id" serial PRIMARY KEY,
+  "frist_name" varchar(100) NOT NULL,
+  "last_name" varchar(100),
+  "email" varchar(200) UNIQUE,
+  "created_at" timestamp NOT NULL,
+  "updated_at" timestamp,
+  "last_seen" timestamp
+);
 
 CREATE TABLE "recipes" (
   "id" serial PRIMARY KEY,
   "user_id" serial,
-  "title" text,
-  "description" text,
-  "img" text,
+  "title" text NOT NULL,
+  "description" text NOT NULL,
   "category" varchar(100),
-  "created_at" timestamp,
+  "created_at" timestamp NOT NULL,
   "update_at" timestamp
 );
 
 CREATE TABLE "steps" (
   "id" serial PRIMARY KEY,
   "recipe_id" serial,
-  "step_num" int,
-  "instruction" text,
-  "created_at" timestamp,
+  "step_num" serial,
+  "instruction" text NOT NULL,
+  "duration" interval NOT NULL,
+  "created_at" timestamp NOT NULL,
   "updated_at" timestamp
 );
 
 CREATE TABLE "ingredients" (
   "id" serial PRIMARY KEY,
   "recipe_id" serial,
-  "ingredient" text,
-  "created_at" timestamp,
+  "ingredient" text NOT NULL,
+  "created_at" timestamp NOT NULL,
   "updated_at" timestamp
-);
-
-CREATE TABLE "users" (
-  "id" serial PRIMARY KEY,
-  "frist_name" varchar(100),
-  "last_name" varchar(100),
-  "created_at" timestamp,
-  "updated_at" timestamp,
-  "last_seen" timestamp
-);
-
-CREATE TABLE "bookmarks" (
-  "id" serial PRIMARY KEY,
-  "recipe_id" serial,
-  "user_id" serial,
-  "created_at" timestamp
-);
-
-CREATE TABLE "follows" (
-  "id" serial PRIMARY KEY,
-  "followed_user" serial,
-  "following_user" serial,
-  "creaed_at" timestamp
-);
-
-CREATE TABLE "likes" (
-  "id" serial PRIMARY KEY,
-  "recipe_id" serial,
-  "user_id" serial,
-  "creaed_at" timestamp
 );
 
 CREATE TABLE "reviews" (
   "id" serial PRIMARY KEY,
   "recipe_id" serial,
   "user_id" serial,
-  "rating" int,
+  "rating" int NOT NULL,
   "comment" text,
-  "creaed_at" timestamp,
+  "created_at" timestamp NOT NULL,
   "update_at" timestamp
+);
+
+CREATE TABLE "likes" (
+  "id" serial PRIMARY KEY,
+  "recipe_id" serial,
+  "user_id" serial,
+  "creaed_at" timestamp NOT NULL
+);
+
+CREATE TABLE "bookmarks" (
+  "id" serial PRIMARY KEY,
+  "recipe_id" serial,
+  "user_id" serial,
+  "created_at" timestamp NOT NULL
+);
+
+CREATE TABLE "follow" (
+  "id" serial PRIMARY KEY,
+  "followed_user" serial,
+  "following_user" serial,
+  "creaed_at" timestamp NOT NULL
+);
+
+CREATE TABLE "user_images" (
+  "id" serial PRIMARY KEY,
+  "user_id" serial,
+  "user_img" text NOT NULL,
+  "created_at" timestamp NOT NULL
+);
+
+CREATE TABLE "recipe_images" (
+  "id" serial PRIMARY KEY,
+  "recipe_id" serial,
+  "thumbnail" text NOT NULL,
+  "ingredient_img" text,
+  "created_at" timestamp NOT NULL
+);
+
+CREATE TABLE "step_images" (
+  "id" serial PRIMARY KEY,
+  "step_id" serial,
+  "step_img" text NOT NULL,
+  "created_at" timestamp NOT NULL
 );
 
 ALTER TABLE "recipes" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
@@ -74,26 +95,24 @@ ALTER TABLE "steps" ADD FOREIGN KEY ("recipe_id") REFERENCES "recipes" ("id");
 
 ALTER TABLE "ingredients" ADD FOREIGN KEY ("recipe_id") REFERENCES "recipes" ("id");
 
-ALTER TABLE "bookmarks" ADD FOREIGN KEY ("recipe_id") REFERENCES "recipes" ("id");
+ALTER TABLE "reviews" ADD FOREIGN KEY ("recipe_id") REFERENCES "recipes" ("id");
 
-ALTER TABLE "bookmarks" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
-
-ALTER TABLE "follows" ADD FOREIGN KEY ("followed_user") REFERENCES "users" ("id");
-
-ALTER TABLE "follows" ADD FOREIGN KEY ("following_user") REFERENCES "users" ("id");
+ALTER TABLE "reviews" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 
 ALTER TABLE "likes" ADD FOREIGN KEY ("recipe_id") REFERENCES "recipes" ("id");
 
 ALTER TABLE "likes" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 
-ALTER TABLE "reviews" ADD FOREIGN KEY ("recipe_id") REFERENCES "recipes" ("id");
+ALTER TABLE "bookmarks" ADD FOREIGN KEY ("recipe_id") REFERENCES "recipes" ("id");
 
-ALTER TABLE "reviews" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE "bookmarks" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 
+ALTER TABLE "follow" ADD FOREIGN KEY ("followed_user") REFERENCES "users" ("id");
 
--- Adding emali to user with unique constrain (forgot to add it in the init)
-ALTER TABLE "users" ADD "email" varchar(200) UNIQUE;
+ALTER TABLE "follow" ADD FOREIGN KEY ("following_user") REFERENCES "users" ("id");
 
--- Adding time field for each steps
+ALTER TABLE "user_images" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 
-ALTER TABLE "steps" ADD "duration" INTERVAL NOT NULL;
+ALTER TABLE "recipe_images" ADD FOREIGN KEY ("recipe_id") REFERENCES "recipes" ("id");
+
+ALTER TABLE "step_images" ADD FOREIGN KEY ("step_id") REFERENCES "steps" ("id");
